9b43dd5be092a5b946409b4d68acf892c0493a8e
Thakur-Avi <thakuravinash409@gmail.com>
2024-05-19 00:25:11 +0530

    implementing image capture

--- app/src/main/java/com/example/procam/MainActivity.kt
+++ app/src/main/java/com/example/procam/MainActivity.kt
@@ -1,35 +1,79 @@
 package com.example.procam
 
+import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.*
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var previewView: PreviewView
+    private lateinit var previewView: PreviewView
+    private lateinit var imageCapture: ImageCapture
+    private lateinit var captureButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        get_permission()
+        getPermission()
 
         previewView = findViewById(R.id.previewView)
+        captureButton = findViewById(R.id.captureButton)
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        cameraProviderFuture.addListener(Runnable {
+        cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
             bindCameraUseCases(cameraProvider)
         }, ContextCompat.getMainExecutor(this))
+
+        captureButton.setOnClickListener { takePhoto() }
     }
 
-    fun bindCameraUseCases(cameraProvider: ProcessCameraProvider) {
+    private fun takePhoto() {
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "$name.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/CameraX-Images")
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            .build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Log.d(TAG, "Photo capture succeeded: ${outputFileResults.savedUri}")
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+            }
+        )
+    }
+
+    private fun bindCameraUseCases(cameraProvider: ProcessCameraProvider) {
         val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
         val preview : Preview = Preview.Builder().build()
@@ -39,15 +83,21 @@ class MainActivity : AppCompatActivity() {
         cameraProvider.bindToLifecycle(this, cameraSelector, preview)
     }
 
-    fun get_permission() {
+
+    private fun getPermission() {
         val permissionList = mutableListOf<String>()
 
         if(checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
             permissionList.add(android.Manifest.permission.CAMERA)
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-                permissionList.add(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if (!Environment.isExternalStorageManager()) {
+                    val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                    intent.data = Uri.parse("package:${applicationContext.packageName}")
+                    startActivity(intent)
+                }
+            }
         }
         else {
             if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
@@ -62,6 +112,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -72,7 +126,7 @@ class MainActivity : AppCompatActivity() {
 
         for (it in grantResults) {
             if(it != PackageManager.PERMISSION_GRANTED)
-                get_permission()
+                getPermission()
         }
     }
 
